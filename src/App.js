4b4705942584585ef95d5112f8b701a667bff066
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2021-10-08 18:32:04 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <h1>hallo</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import '.comp-basis/index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 import App from './App';
-import reportWebVitals from '../comp-basis/reportWebVitals';
+import reportWebVitals from './reportWebVitals'
+import {createStore} from 'redux'
+//Store 
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
+//Action
+const increment = () => {
+  return {
+    type:"INCREMENT"
+  }
+}
+const decrement = () => {
+  return {
+    type:"DECREMENT"
+  }
+}
+
+//Reducer
+const counter = (state = 0, action) => {
+    switch(action.type) {
+      case 'INCREMENT':
+        return state + 1;
+      case 'DECREMENT':
+        return state - 1; 
+        default:
+        break;
+ }
+
+}
+
+let store = createStore(counter);
+//display it to the console
+store.subscribe(()=> console.log(store.getState()))
+
+
+//dispatch
+store.dispatch(increment());
+
+
+
+
+
+
+
+ReactDOM.render(<React.StrictMode> <App /></React.StrictMode>,document.getElementById('root'));
 reportWebVitals();
